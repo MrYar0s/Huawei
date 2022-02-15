448022d4510c7d0a107bf1bc259004ef70d1cbbe
MrYar0s <molokan.yaroslavik@gmail.com>
2022-02-15 17:57:14 +0300

    Delete build

--- Queue/build/tests/CMakeFiles/run_list_test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_list_test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_list_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Queue/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mryaros/Huawei/Queue/tests
-# Build directory: /home/mryaros/Huawei/Queue/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
